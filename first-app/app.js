337e877efe3f5e8ab6914cd294118a90a4c57497
Morteza Bakhshandeh <bcfcode@gmail.com>
2024-10-23 21:18:18 +0330

    18. Event Arguments

--- first-app/app.js
+++ first-app/app.js
@@ -1,1 +1,12 @@
-console.log(module)
+const EventEmitter = require('node:events');
+const emitter = new EventEmitter()
+
+// Register a listener 
+emitter.on('messageLogged', ({ id, url }) => {
+  console.log('Listener called', id, url)
+})
+
+// Raise an event
+emitter.emit('messageLogged', { id: 1, url: 'http://' })
+
+// Raise: logging (data: message)
